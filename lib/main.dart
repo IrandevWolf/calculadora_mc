20b7b32091b454a43de083c27a1047602ed9c131
Iranildo Borges <167035593+IrandevWolf@users.noreply.github.com>
2024-05-18 14:38:10 -0300

    add Controller e Cálculos..

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,38 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+  String _infoText = "Informe seus dados!";
+
+  void _resetFields() {
+    weightController.text = "";
+    heightController.text = "";
+    _infoText = "Informe seus dados!";
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+
+      if (imc < 18.6) {
+        _infoText = "Abaixo do peso(${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente Acima do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,12 +56,12 @@ class _HomeState extends State<Home> {
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.refresh),
-            onPressed: () {},
+            onPressed: _resetFields,
           ),
         ],
       ),
       backgroundColor: Colors.white,
-      body:  SingleChildScrollView(
+      body: SingleChildScrollView(
         padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -42,6 +74,7 @@ class _HomeState extends State<Home> {
                   labelStyle: TextStyle(color: Colors.blueGrey)),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.blueGrey, fontSize: 28.0),
+              controller: weightController,
             ),
             TextField(
               keyboardType: TextInputType.number,
@@ -50,24 +83,28 @@ class _HomeState extends State<Home> {
                   labelStyle: TextStyle(color: Colors.blueGrey)),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.blueGrey, fontSize: 28.0),
+              controller: heightController,
             ),
             Padding(
-              padding:EdgeInsets.only(top: 10.0,bottom: 10.0),
-                child :Container(
-                    height: 50.0,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text("Calcular" , style: TextStyle(color: Colors.white, fontSize: 28.0)
-                        ,),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blueGrey,
-                      ),
-                    )
-                ),
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Container(
+                  height: 50.0,
+                  child: ElevatedButton(
+                    onPressed: _calculate,
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(color: Colors.white, fontSize: 28.0),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueGrey,
+                    ),
+                  )),
             ),
-            Text("OLA",
+            Text(
+              _infoText,
               textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.blueGrey, fontSize: 28.0),)
+              style: TextStyle(color: Colors.blueGrey, fontSize: 28.0),
+            )
           ],
         ),
       ),
